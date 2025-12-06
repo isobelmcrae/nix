d596c4b6ec1a231959168ec9499000a1b53539b2
isobelmcrae <isobel@isobel.zip>
2025-12-07 01:48:37 +1100

    fonts: add noto-fonts

--- configuration.nix
+++ configuration.nix
@@ -2,17 +2,21 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
-  imports =
-    [ 
-      ./hardware-configuration.nix
-      ./apple-silicon-support
-      ./home.nix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+    ./apple-silicon-support
+    ./home.nix
+  ];
 
-  # asahi quirks 
+  # asahi quirks
   hardware.asahi.peripheralFirmwareDirectory = ./firmware;
   boot.extraModprobeConfig = ''
     options hid_apple iso_layout=0
@@ -21,16 +25,16 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  
+
   # networking
   networking.hostName = "kiwi";
   networking.networkmanager.enable = true;
-	
+
   # uv installs python binaries to ~/.local/bin
   environment.localBinInPath = true;
   environment.variables = {
-	  XDG_CURRENT_DESKTOP = "niri";
-	  XDG_SESSION_TYPE = "wayland";
+    XDG_CURRENT_DESKTOP = "niri";
+    XDG_SESSION_TYPE = "wayland";
   };
 
   # more stuff to get uv to work
@@ -40,26 +44,30 @@
   time.timeZone = "Australia/Sydney";
 
   users.users.iso = {
-     isNormalUser = true;
-     extraGroups = [ "wheel" "networkmanager" ];
-     shell = pkgs.fish;
-     packages = with pkgs; [
-       tree
-     ];
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+    ];
+    shell = pkgs.fish;
   };
 
   programs.niri.enable = true;
   programs.fish.enable = true;
 
   environment.systemPackages = with pkgs; [
-     openssl
-     vim
-     git
-     wget
+    openssl
+    vim
+    git
+    wget
+    gcc
+    clang
   ];
 
   fonts.packages = with pkgs; [
-	  nerd-fonts.jetbrains-mono
+    nerd-fonts.jetbrains-mono
+    noto-fonts
+    noto-fonts-cjk-sans
   ];
 
   # programs.mtr.enable = true;
@@ -71,4 +79,3 @@
   services.openssh.enable = true;
   system.stateVersion = "25.11";
 }
-
